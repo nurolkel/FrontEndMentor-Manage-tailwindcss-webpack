1f55460546bf8147993de09c877ada9976f7236a
Kelvin Nunez <knunezweb@gmail.com>
2022-06-10 14:52:47 -0400

    Added Carousel

--- src/app.js
+++ src/app.js
@@ -8,3 +8,26 @@ btn.addEventListener('click',() => {
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 })
+
+
+const buttons = document.querySelectorAll('[data-carousel-button]')
+
+buttons.forEach(btn => {
+    btn.addEventListener('click',() => {
+        const offset = btn.dataset.carouselButton === 'next' ? 1 : -1;
+        const slides = btn.closest('[data-carousel]').querySelector('[data-slides]')
+
+        console.log(slides);
+        console.log(btn.closest('[data-carousel]'))
+        
+        const activeSlide = slides.querySelector('[data-active]');
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        console.log(slides.children)
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active
+
+    })
+})
